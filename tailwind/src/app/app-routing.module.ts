e4755af08e8b7300105d35272764c161f7f9a3b0
Nightwing-Dev <diego77andre77@gmail.com>
2022-06-18 23:53:12 -0500

    creacion tarjetas finalizada & creacion interface de users y servicio users

--- tailwind/src/app/app-routing.module.ts
+++ tailwind/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   {
     path: 'posts', loadChildren:
       () => import('./posts/posts.module').then(m => m.PostsModule)
   },
+  { path: 'users', component: UsersComponent }
 ];
 
 @NgModule({
